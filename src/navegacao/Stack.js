858d0a30f42aa4fd0aa212fad814ef2ac4607edc
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2020-08-12 12:46:21 -0300

    Navegação básica entre componentes

---
+++ src/navegacao/Stack.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { createStackNavigator} from '@react-navigation/stack'
+import TelaA from '../views/TelaA'
+import TelaB from '../views/TelaB'
+import TelaC from '../views/TelaC'
+import PassoStack from '../components/PassoStack'
+
+// Cria a navegação do tipo stack
+const Stack = createStackNavigator()
+
+export default props => (
+    // Cria um componente de navegação e define a rota inicial do app
+    <Stack.Navigator initialRouteName="TelaA" screenOptions={{
+        headerShown: true
+    }}>
+        {/* Cria as telas de nvaegação e suas opções*/}
+        <Stack.Screen name="TelaA" options={{title: 'Informações Iniciais'}}>
+            {props => (
+                <PassoStack {...props} avancar="TelaB">
+                    <TelaA />
+                </PassoStack>
+            )}
+        </Stack.Screen>
+        <Stack.Screen name="TelaB">
+            {props => (
+                <PassoStack {...props} avancar="TelaC" voltar>
+                    <TelaB />
+                </PassoStack>
+            )}
+        </Stack.Screen>
+        <Stack.Screen name="TelaC">
+            {props => (
+                <PassoStack {...props} voltar>
+                    <TelaC />
+                </PassoStack>
+            )}
+        </Stack.Screen>
+
+    </Stack.Navigator>
+)
